b575b72a77cd36f8b05f3e7f01f364190425ebd3
fernanda pizarro <fpizarroseverino424@gmail.com>
2025-12-22 17:50:01 -0300

    Import os module for environment variable access

--- backend/auth.py
+++ backend/auth.py
@@ -4,7 +4,7 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-
+import os
 from database import get_db
 from model.models import User
 
